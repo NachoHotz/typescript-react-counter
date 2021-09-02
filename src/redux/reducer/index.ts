f44741abb1f2e83642cc680d2c0be469ae6943c1
Nacho Hotz <juanhotz611@gmail.com>
2021-09-02 00:32:18 -0300

    modify reducer

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -5,8 +5,8 @@ const initialState = 0;
 
 const rootReducer = (state = initialState, action: Action) => {
   switch(action.type) {
-    case ActionType.INCREMNET : return state + action.payload;
-    case ActionType.DECREMENT: return state - action.payload;
+    case ActionType.INCREMNET : return state + 1;
+    case ActionType.DECREMENT: return state - 1;
     case ActionType.RESET: return state = 0;
     default: return state;
   }
