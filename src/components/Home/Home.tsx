c9a4dfdc23a09e80e47055b686e7479d50c38fd3
Nacho Hotz <juanhotz611@gmail.com>
2021-09-02 00:51:38 -0300

    finish Home component and center div

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, reset } from '../../redux/actions/types';
+import style from './Home.module.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state);
+
+  return (
+    <div className={style.container}>
+      <h3>{counter}</h3>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
 }
