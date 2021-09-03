b7ab12ea48e3db91011c66974623a965b0369da8
Nacho Hotz <juanhotz611@gmail.com>
2021-09-03 01:07:50 -0300

    fix import in Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement, reset } from '../../redux/actions/types';
+import { increment, decrement, reset } from '../redux/actions/types';
 import style from './Home.module.css';
 
 export default function Home() {
