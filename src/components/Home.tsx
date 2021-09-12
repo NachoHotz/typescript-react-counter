4fcfd5af9e1dfaee8a38ab557efecabcb97f31e3
Nacho Hotz <juanhotz611@gmail.com>
2021-09-12 18:24:47 -0300

    fix redux state type error

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { increment, decrement, reset } from '../redux/actions/types';
 import style from './Home.module.css';
 
 export default function Home() {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state);
+  const counter = useSelector((state: RootStateOrAny) => state.counter);
 
   return (
     <div className={style.container}>
