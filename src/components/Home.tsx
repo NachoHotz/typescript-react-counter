fe4249c0409bf453656b2793d38479e89d80afc2
Nacho Hotz <juanhotz611@gmail.com>
2021-09-12 19:01:54 -0300

    fix actions imports

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,24 +1,18 @@
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as actionCreators from '../redux/actions/types';
+import * as actions from '../redux/actions/types';
 import style from './Home.module.css';
 
 export default function Home() {
   const dispatch = useDispatch();
   const counter: number = useSelector((state: RootStateOrAny) => state.counter);
 
-  const { increment, decrement, reset } = bindActionCreators(
-    actionCreators,
-    dispatch,
-  );
-
   return (
     <div className={style.container}>
       <p className={style.counter}>{counter}</p>
       <div>
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => dispatch(actions.increment())}>Increment</button>
+        <button onClick={() => dispatch(actions.decrement())}>Decrement</button>
+        <button onClick={() => dispatch(actions.reset())}>Reset</button>
       </div>
     </div>
   );
