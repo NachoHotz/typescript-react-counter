c46f3468629a93c1e4c922cb21c97508181610ae
Nacho Hotz <juanhotz611@gmail.com>
2021-09-12 19:57:50 -0300

    add state interface file

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,21 +1,18 @@
 import { Action } from './interface';
 import { ActionType } from '../actions/names';
-
-interface InitialState {
-  counter: number,
-}
+import { InitialState } from './stateInterface';
 
 const initialState: InitialState = {
   counter: 0,
-}
+};
 
 const rootReducer = (state = initialState, action: Action) => {
-  switch(action.type) {
-    case ActionType.INCREMNET : return { ...state, counter: state.counter + 1 };
-    case ActionType.DECREMENT: return { ...state, counter: state.counter - 1 } ;
-    case ActionType.RESET: return { ...state, counter: state.counter = 0 };
+  switch (action.type) {
+    case ActionType.INCREMNET: return { ...state, counter: state.counter + 1 };
+    case ActionType.DECREMENT: return { ...state, counter: state.counter - 1 };
+    case ActionType.RESET: return { ...state, counter: (state.counter = 0) };
     default: return state;
   }
-}
+};
 
 export default rootReducer;
